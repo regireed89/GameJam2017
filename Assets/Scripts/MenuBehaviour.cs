ffc142f52c52ece87f27d2bff51d5cdb0cf39012
Nicholas Arnaud <arnaudnicholas@gmail.com>
2017-10-26 11:46:12 -0500

    Added UI and Menu Behaviours

---
+++ Assets/Scripts/MenuBehaviour.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuBehaviour : MonoBehaviour
+{
+    public void NewGame()
+    {
+        SceneManager.LoadScene("0.Main");
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("1.MainMenu");
+    }
+
+    public void Credits()
+    {
+        SceneManager.LoadScene("2.Credits");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void Resume(GameObject panel)
+    {
+        panel.GetComponent<Image>().enabled = false;
+        panel.GetComponentInChildren<Text>().enabled = false;
+        foreach (var button in panel.GetComponentsInChildren<Button>())
+        {
+            button.gameObject.SetActive(false);
+        }
+        Time.timeScale = 1.0f;
+    }
+}
