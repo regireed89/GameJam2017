f01c862b4ab614925c07e8c8f5aeca633c7efce3
Nicholas Arnaud <arnaudnicholas@gmail.com>
2017-10-27 09:43:12 -0500

    Setup for Animations

--- Assets/Scripts/EnemyBehavior.cs
+++ Assets/Scripts/EnemyBehavior.cs
@@ -3,13 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class EnemyBehavior : MonoBehaviour{
+public class EnemyBehavior : MonoBehaviour
+{
+    private Animator anim;
 
-    private void OnTriggerEnter(Collider other)
+    void Start()
     {
+         anim= GetComponent<Animator>();
+        anim.SetBool("Running",true);
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        anim.SetTrigger("Attack");
         if (other.tag == "Player")
-            SceneManager.LoadScene("1.MainMenu");
+        {
+            StartCoroutine(GameOver());
+        }
+           
     }
 
-
+    IEnumerator GameOver()
+    {
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene("1.MainMenu");
+    }
 }
